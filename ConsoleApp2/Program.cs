6da4fedde1e751a557af7e7eb65c145315d6f83a
Anderson Domingos <anderson@mica12.com.br>
2023-08-23 16:35:41 -0300

    atualização de arquivos da app para torná-la pública

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int i;
+
+int b = 8, a = 32;
+
+for (i = 0; i <= 10; i++)
+
+{
+
+    if ((a / b * 2)== 2)
+
+    {
+
+        Console.WriteLine( i + " ");
+
+        continue;
+
+    }
+
+    else if (i != 4)
+
+        Console.WriteLine(i + " ");
+
+    else
+
+        break;
+
+}
+
+Console.ReadLine();
